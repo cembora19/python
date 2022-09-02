bc1fe605d6ee4a7c7f77f450cfd1b3601c3559e1
cembora19 <cem_bora_5-c@hotmail.com>
2022-09-02 16:20:50 +0300

    eyyo

---
+++ day24IntermediateFilesDirectoriesPaths/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+with open(f"{PATH}my_file.txt") as file:
+    contents=file.read()
+    print(contents)
--- day24v2/main.py
+++ day24v2/main.py
@@ -1,8 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+PLACEHOLDER="[name]"
+
+NAME_PATH="\\Users\\cembora\\Desktop\\python\\day24v2\\Input\\Names\\invited_names.txt"
+
+LETTER_PATH="\\Users\\cembora\\Desktop\\python\\day24v2\\Input\\Letters\\starting_letter.txt"
+
+SEND_PATH="\\Users\\cembora\\Desktop\\python\\day24v2\\Output\\ReadyToSend\\"
+
+with open(f"{NAME_PATH}") as names_file:
+    names=names_file.readlines()
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open(f"{LETTER_PATH}") as letter_file:
+    letter_contents=letter_file.read()
+
+    for name in names:
+        stripped_name=name.strip()
+        new_letter=letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"{SEND_PATH}letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+        
